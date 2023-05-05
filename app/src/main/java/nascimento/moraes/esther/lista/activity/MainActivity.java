8c87628ed53c9b3e4a14b0b599f230ebf83efc4b
20211tiimi0052 <esther.ifes2021@gmail.com>
2023-05-05 08:55:27 -0300

    Criação do APP 4 => Lista PT.2

--- app/src/main/java/nascimento/moraes/esther/lista/activity/MainActivity.java
+++ app/src/main/java/nascimento/moraes/esther/lista/activity/MainActivity.java
@@ -1,17 +1,17 @@
 package nascimento.moraes.esther.lista.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
